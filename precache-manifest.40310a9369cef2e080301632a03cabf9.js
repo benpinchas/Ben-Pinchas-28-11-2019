63d0f5cacb390506055777b599df06d6b6730edb
Ben Pinchas <benphs5@gmail.com>
2019-12-01 03:24:32 +0200

    Updates

---
+++ precache-manifest.40310a9369cef2e080301632a03cabf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62d9d66b3fee0961900e80d4efa99ab",
+    "url": "/Ben-Pinchas-28-11-2019/index.html"
+  },
+  {
+    "revision": "6d040ac64c87354f2e80",
+    "url": "/Ben-Pinchas-28-11-2019/static/css/main.55c43fa4.chunk.css"
+  },
+  {
+    "revision": "7f0a8ea6dee46a6a3e84",
+    "url": "/Ben-Pinchas-28-11-2019/static/js/2.f9a34d7f.chunk.js"
+  },
+  {
+    "revision": "6d040ac64c87354f2e80",
+    "url": "/Ben-Pinchas-28-11-2019/static/js/main.b209b1a8.chunk.js"
+  },
+  {
+    "revision": "4018b4bf1a0081fcb782",
+    "url": "/Ben-Pinchas-28-11-2019/static/js/runtime-main.978be1c7.js"
+  }
+]);
